0c75deed0288370f199f51ab678cd5c91baae76b
¨kevin180317¨ <ortega1gv@gmail.com>
2025-07-24 11:51:24 -0700

    update shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Nav from "@/components/Nav";
 import React from "react";
+import DockDemo from "@/components/Dock";
+import Hero from "@/components/Hero";
 
 function page() {
   return (
     <main>
       <Nav />
+      <Hero />
+      <div className="fixed bottom-10 left-1/2 -translate-x-1/2">
+        <DockDemo />
+      </div>
     </main>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Form } from "./Form";
+
+function Hero() {
+  return <div>{/* <Form /> */}</div>;
+}
+
+export default Hero;
